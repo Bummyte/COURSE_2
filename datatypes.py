674778137547d0ad5201b15f603547e89d260f30
Bunmi <ugugebunmi@gmail.com>
2023-07-28 23:40:18 +0000

    first commit

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+#for integral
+A = 4 
+print (A)
+print (type(A))
+
+#for string datatype
+B = "Hello World"
+print (B)
+print (type(B))
+
+#for float datatype
+C = 15.5
+print (C)
+print (type(C))
+
+#for complex datatype
+D = 2j 
+type_D = type(D)
+print(f"the value of D is {D}, and the data type of D is {type_D}")
+
+#list datatype
+X = ["Love", "Peace", "Joy"]
+type_X = type(X)
+print(f"the value of x is {X},and the datatype is {type_X}")
+
+#for range datatype
+Y = range(5)
+type_Y = type(Y)
+print(f"the value of Y is {Y}, and the datatype is {type_Y}")
+
+
+
+
+
+
---
+++ fullname.py
@@ -0,0 +1,14 @@
+FirstName = "Bunmi"
+MiddleName = "Lydia"
+LastName = "Uguge"
+FullName = FirstName + " " + MiddleName + " " +  LastName
+print(f"the value of FullName is {FullName}")
+print("the FirstName of the user is {}, the MiddleName of the user is {} and the LastName of the user is {}". format(FirstName, MiddleName, LastName))
+
+
+FirstName = "Bunmi"
+MiddleName = "Lydia"
+LastName = "Uguge"
+FullName = FirstName + " " + MiddleName + " " +  LastName
+print(f"the value of FullName is {FullName}")
+print("the FirstName of the user is {}, the MiddleName of the user is {} and the LastName of the user is {}". format(FirstName, MiddleName, LastName))
